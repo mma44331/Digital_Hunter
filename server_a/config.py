457f98545e2ca925376283c5526e4758fdfb763e
meyir moshe <mma088651259@gmail.com>
2026-03-16 16:45:01 +0200

    adding compose

--- server_a/config.py
+++ server_a/config.py
@@ -5,8 +5,7 @@ class Config:
     def __init__(self):
         self.bootstrap_server = os.getenv('BOOTSTRAP_SERVER','kafka:9092')
         self.topic_intel = os.getenv('TOPIC_INTEL','intel')
-        self.topic_attack = os.getenv('TOPIC_ATTACK','attack')
-        self.topic_damage = os.getenv('TOPIC_DAMAGE','damage')
-        self.topic_intel_signals_dlq = os.getenv('INTEL_SIGNALS_DLQ','intel_signals_dlq')
-
+        self.topic_intel_signals_dlq = os.getenv('TOPIC_INTEL_SIGNALS_DLQ','intel_signals_dlq')
+        self.db_name = os.getenv('DB_NAME','target_bank')
+        self.coll_name = os.getenv('COLL_NAME','targets')
 
